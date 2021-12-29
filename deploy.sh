b103d7c501b5f46c2f444e332bec08e352d60f22
Gérald LEBAN <gerald.leban0@gmail.com>
2021-12-29 19:50:46 +0100

    Shell script to deploy

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-scp index.html style.css leban@etud.insa-toulouse.fr:public_html/projet-prog-web/
+scp -r index.html style.css 2048 demineur morpion rps taquin leban@etud.insa-toulouse.fr:public_html/projet-prog-web/
